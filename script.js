c2299701cbd2767a32afd8ecb2f09c007741cce4
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-11-13 18:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-//your JS code here. If required.
+let name_regex = /^[A-Za-z]{3,}$/;
+let email_regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+let phone_regex = /^[0-9]{10}$/;
+
+let inputs = document.querySelectorAll('input');
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let value = e.target.value;
+
+    if (e.target.type === "text") {
+      if (name_regex.test(value)) {
+        e.target.style.backgroundColor = "#8CE489";
+      } else {
+        e.target.style.backgroundColor = "#F7B5BD";
+      }
+    }
+
+    else if (e.target.type === "email") {
+      if (email_regex.test(value)) {
+        e.target.style.backgroundColor = "#8CE489";
+      } else {
+        e.target.style.backgroundColor = "#F7B5BD";
+      }
+    }
+
+    else if (e.target.type === "number" || e.target.type === "tel") {
+      if (phone_regex.test(value)) {
+        e.target.style.backgroundColor = "#8CE489";
+      } else {
+        e.target.style.backgroundColor = "#F7B5BD";
+      }
+    }
+  });
+});
